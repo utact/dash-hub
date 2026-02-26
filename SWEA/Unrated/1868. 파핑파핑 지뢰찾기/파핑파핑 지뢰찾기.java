5ba49aafd9ddd2cbd5280286edeb86e138da570d
Yoo Seung-jun <funczun@gmail.com>
2026-02-26 23:34:57 +0900

    [Unrated] Title: 파핑파핑 지뢰찾기, Time: 178  ms, Memory: 44588  KB - DashHub

--- SWEA/Unrated/1868. 파핑파핑 지뢰찾기/파핑파핑 지뢰찾기.java
+++ SWEA/Unrated/1868. 파핑파핑 지뢰찾기/파핑파핑 지뢰찾기.java
@@ -1,10 +1,11 @@
 import java.io.*;
+import java.util.*;
 
 public class Solution {
 	static int[] dr = { -1, -1, -1, 0, 0, 1, 1, 1 };
 	static int[] dc = { -1, 0, 1, -1, 1, -1, 0, 1 };
 	static int N, ans;
-	static int[][] map, vst;
+	static int[][] ips, map, vst;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,18 +16,18 @@ public class Solution {
 		for (int tc = 1; tc <= T; tc++) {
 			N = Integer.parseInt(br.readLine());
 			ans = N * N;
+			ips = new int[N][N];
 			map = new int[N][N];
 			vst = new int[N][N];
 
 			for (int r = 0; r < N; r++) {
 				String line = br.readLine();
 				for (int c = 0; c < N; c++) {
-					map[r][c] = line.charAt(c);
+					ips[r][c] = line.charAt(c);
 				}
 			}
 
 			toNum();
-
 			game();
 
 			sb.append('#').append(tc).append(' ').append(ans).append('\n');
@@ -38,9 +39,7 @@ public class Solution {
 	static void toNum() {
 		for (int r = 0; r < N; r++) {
 			for (int c = 0; c < N; c++) {
-				if (map[r][c] == '.') {
-					map[r][c] = 0;
-				} else {
+				if (ips[r][c] != '.') {
 					vst[r][c] = 1;
 					ans--;
 					for (int i = 0; i < 8; i++) {
@@ -59,24 +58,49 @@ public class Solution {
 	}
 
 	static void game() {
+		Queue<int[]> q = new ArrayDeque<int[]>();
+
 		for (int r = 0; r < N; r++) {
 			for (int c = 0; c < N; c++) {
 				if (vst[r][c] == 0 && map[r][c] == 0) {
-					for (int i = 0; i < 8; i++) {
-						int nr = r + dr[i];
-						int nc = c + dc[i];
-
-						if (nr < 0 || nr >= N || nc < 0 || nc >= N || vst[nr][nc] == 1) {
-							continue;
-						}
-
-						if (map[nr][nc] == 0) {
+					q.add(new int[] {r, c});
+					
+					while (!q.isEmpty()) {
+						int[] cur = q.remove();
+						int cr = cur[0];
+						int cc = cur[1];
+						
+						vst[r][c] = 1;
+						
+						for (int i = 0; i < 8; i++) {
+							int nr = cr + dr[i];
+							int nc = cc + dc[i];
+							
+							if (nr < 0 || nr >= N || nc < 0 || nc >= N || vst[nr][nc] == 1) {
+								continue;
+							}
+							
+							if (map[nr][nc] == 0) {
+								q.add(new int[] {nr, nc});
+							}
+							
 							vst[nr][nc] = 1;
+							
 							ans--;
 						}
 					}
 				}
 			}
+		}
+		
+	}
+	
+	static void printMap(int[][] map) {
+		for (int r = 0; r < map.length; r++) {
+			for (int c = 0; c < map.length; c++) {
+				System.out.print(map[r][c] + " ");
+			}
+			System.out.println();
 		}
 	}
 }
